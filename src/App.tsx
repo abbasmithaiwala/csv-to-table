fac1cd7226e34ba53d81e8933d18c29ca96d2252
abbasmithaiwala <mithaiwalaabbas1@gmail.com>
2025-04-04 16:18:05 +0530

    Updated app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,50 @@
-import './App.css'
+import { useState } from 'react';
+import { 
+  ThemeProvider, 
+  createTheme, 
+  CssBaseline,
+  Box,
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+} from '@mui/material';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+import { TableDemo } from './pages/TableDemo';
+import './App.css';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  
+  const theme = createTheme({
+    palette: {
+      mode: darkMode ? 'dark' : 'light',
+    },
+  });
 
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  )
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Data Table
+            </Typography>
+            <IconButton 
+              color="inherit" 
+              onClick={() => setDarkMode(!darkMode)}
+              sx={{ ml: 1 }}
+            >
+              {darkMode ? <Brightness7Icon /> : <Brightness4Icon />}
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <TableDemo />
+      </Box>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
